00bb6d7147eb4e95c41d66c313bbb9785ab1a070
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-01-02 13:32:09 -0800

    menu taking shape

--- app/controllers/color_controller.rb
+++ app/controllers/color_controller.rb
@@ -2,7 +2,6 @@ class ColorController < ViewController::Portrait
   
   outlet :drawing_container
   outlet :page_image
-  outlet :menu_button
   outlet :menu_view
 
   def viewDidLoad
@@ -13,14 +12,10 @@ class ColorController < ViewController::Portrait
   end
 
   def show_menu sender
-    @drawing.disable
-    @menu_button.hide
     @menu_view.show
   end
 
   def close_menu sender
-    @drawing.enable
-    @menu_button.show
     @menu_view.hide
   end
 
